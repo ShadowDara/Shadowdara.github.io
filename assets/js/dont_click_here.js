251a5f2c01d2dbf36d7d61151d8a92f860b46856
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-02-27 21:26:35 +0100

    save

---
+++ assets/js/dont_click_here.js
@@ -0,0 +1,31 @@
+function showConfirm() {
+    let result = confirm("Are you sure?");
+    
+    if (result) {
+      showConfirm2()
+    }
+    
+    else {
+      denied()
+    }
+}
+
+function showConfirm2() {
+    let result = confirm("Are you really sure?");
+    
+    if (result) {
+      showConfirm3()
+    }
+    
+    else {
+      denied()
+    }
+}
+
+function showConfirm3() {
+
+}
+
+function denied() {
+    alert("I knew, you are not sure!")
+}
