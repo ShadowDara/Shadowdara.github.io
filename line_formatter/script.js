56168b7f281097f10fe04a57fdd49e73c0c56f65
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-04-24 18:06:30 +0200

    Update script.js

--- line_formatter/script.js
+++ line_formatter/script.js
@@ -1,29 +1,24 @@
-// Funktion zum Formatieren des Textes auf eine einzige Zeile
 function formatTextToSingleLine(text) {
     let formattedText = text.replace(/\t/g, '\\t').replace(/\n/g, '\\n');
     return formattedText;
 }
 
 document.getElementById('generate').addEventListener("click", function () {
-    const input1 = document.getElementById('input1');  // Holen des Eingabefelds
-    const content = formatTextToSingleLine(input1.value);  // Zugriff auf den Textwert des Eingabefelds
+    const input1 = document.getElementById('input1');
+    const content = formatTextToSingleLine(input1.value);
 
-    // Ausgabe des formatierten Textes
     document.getElementById('output').innerText = content;
 
-    // Erstelle den "Copy Link"-Button
     const copyButton = document.createElement("button");
     copyButton.id = 'b-copy';
     copyButton.innerText = 'Copy Link';
 
-    // Füge den Button zum DOM hinzu
-    document.getElementById('copy-button').innerHTML = '';  // Leeren des bestehenden Inhalts
+    document.getElementById('copy-button').innerHTML = '';
     document.getElementById('copy-button').appendChild(copyButton);
 
-    // Kopieren-Funktion
     copyButton.addEventListener("click", function () {
         navigator.clipboard.writeText(content).then(function () {
-            copyButton.innerText = "Link copied";  // Button-Text nach dem Kopieren ändern
+            copyButton.innerText = "Link copied";
         }).catch(function (err) {
             console.error("Fehler beim Kopieren: ", err);
         });
