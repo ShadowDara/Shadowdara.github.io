817f3115631ac17afc83ac960e46b22a7969068a
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-05-24 17:51:23 +0200

    Updates

--- html/line_formatter/format.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('generate_sin').addEventListener("click", function () {
-    const input1 = document.getElementById('input1');
-    const content = formatTextToSingleLine(input1.value);
-    do_copy(content)
-});
