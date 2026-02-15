e989eddc5b85f291f0c582e0778e7c3febccad34
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-02-15 22:53:48 +0100

    move stuff to shadowdara.42web.io

--- html/line_formatter/extend.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('generate_mul').addEventListener("click", function () {
-    const input = document.getElementById('input');
-    const content = formatTextTomultiLine(input.value);
-    do_copy(content)
-});
