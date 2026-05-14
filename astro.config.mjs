b161fdea4e3f79db17de20da6dbe67a6b60de72c
ShadowDara <shadowdara74x@gmail.com>
2026-05-14 21:43:54 +0200

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // Astro Config
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -31,9 +32,12 @@ export default defineConfig({
       noExternal: ["samengine"],
     },
   },
+
   // base: '/portfolio',
   base: '',
-  integrations: [mdx(), sitemap()],
+  
+  integrations: [mdx(), sitemap(), svelte()],
+
   site: 'https://shadowdara.github.io',
 
   redirects
